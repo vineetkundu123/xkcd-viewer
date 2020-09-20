83a760ad3172e619369bd56c23462de52f83bc67
Kundu, Vineet <vineet.kundu@dnb.no>
2020-09-20 10:55:57 +0200

    Removed un-necessary scene-delegates, since we are not going to use them for our App. Also, removed dummy ViewController and Main Storyboard, since we are going to develop the interfaces programmatically creating layouts using TinyConstraints Added a new App-communicator to start the app and added empty methiod for loading initial view - Initialised app from AppDelegate

---
+++ xkcd-viewer/xkcd-viewer/AppCommunicator.swift
@@ -0,0 +1,21 @@
+import Foundation
+import UIKit
+
+class AppCommunicator {
+    static let shared = AppCommunicator()
+    public var mainWindow: UIWindow
+    
+    private init() {
+        mainWindow = UIWindow(frame: UIScreen.main.bounds)
+    }
+    
+    func initialiseApp() {
+        //Perform any operations before loading initial page here
+        loadInitialPage()
+    }
+    
+    //Load Initial Page
+    private func loadInitialPage() {
+       //TODO: Load first view controller from here
+    }
+}
